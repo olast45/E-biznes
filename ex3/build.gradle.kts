d55a3bf543d8d71082d05f0caa91306fbdf9ed74
Aleksandra <aleksandra.stepien@4soft.co>
2025-04-06 14:50:57 +0200

    feat: Extend app functionality to receive Discord messages

--- ex3/build.gradle.kts
+++ ex3/build.gradle.kts
@@ -1,4 +1,4 @@
-val ktor_version: String by project
+val ktor_version = "2.3.7"
 
 plugins {
     kotlin("jvm") version "2.1.10"
@@ -10,15 +10,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.kord.dev/snapshots")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("dev.kord:kord-core:0.8.3")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
 }
 
--- ex3/src/main/kotlin/Main.kt
+++ ex3/src/main/kotlin/Main.kt
@@ -7,28 +7,57 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.serialization.Serializable
 import io.github.cdimascio.dotenv.Dotenv
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
 
 val dotenv = Dotenv.load()
 
 @Serializable
 data class Message(val content: String)
 
-suspend fun main() {
-
-    val webhookUrl = dotenv["WEBHOOK"] ?: "default_value"
-    val message = Message("Hello from Ktor framework!")
-
+suspend fun sendMessageToChannel(botToken: String, channelId: String, messageContent: String) {
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json()
         }
     }
 
-    val response: HttpResponse = client.post(webhookUrl) {
-        contentType(ContentType.Application.Json)
+    val message = Message(content = messageContent)
+
+    val response: HttpResponse = client.post("https://discord.com/api/v10/channels/$channelId/messages") {
+        headers {
+            append(HttpHeaders.Authorization, "Bot $botToken")
+            append(HttpHeaders.ContentType, ContentType.Application.Json)
+        }
         setBody(message)
     }
 
-
     client.close()
+}
+
+@OptIn(PrivilegedIntent::class)
+suspend fun main() {
+
+    val token = dotenv["TOKEN"] ?: "default_value"
+    val messageContent = "Hello from Ktor framework!"
+    val channelId = "1357077739412127928"
+    val bot = Kord(token)
+
+
+    bot.on<MessageCreateEvent> {
+        if (message.author?.isBot == true) return@on
+        val msg = message.content
+        println("Received: $msg")
+
+        sendMessageToChannel(token, channelId, messageContent)
+
+    }
+
+    bot.login {
+        intents += Intent.MessageContent
+    }
+
 }
